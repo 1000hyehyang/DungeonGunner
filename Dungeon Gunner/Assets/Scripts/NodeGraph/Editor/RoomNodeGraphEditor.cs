9212e6a64d04f0582d404ae20ce21137ba61bc85
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-03-04 13:31:53 +0900

    lecture.8
    Create a Simple Editor Window

--- Dungeon Gunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Dungeon Gunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,10 +5,37 @@ using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
+    private GUIStyle roomNodeStyle;
+
+    private const float nodeWidth = 160f;
+    private const float nodeHeight = 75f;
+    private const int nodePadding = 25;
+    private const int nodeBorder = 12;
+
     [MenuItem("Room Node Graph Ediotr", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
 
-    private static void GraphWindow()
+    private static void GraphWindow() // 유니티 Window에서 에디터 창을 만들 수 있음
     {
         GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnEnable()
+    {
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node") as Texture2D;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+        
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 1");
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 2");
+        GUILayout.EndArea();
     }
 }
